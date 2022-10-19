401feae10572fa38da0195bd918cb55d09d872d9
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-20 01:46:23 +0530

    Update best of two.cpp

--- best of two.cpp
+++ best of two.cpp
@@ -13,3 +13,29 @@ int main()
         
     return 0;
 }
+
+
+
+//other method 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while (n--)
+    {
+    int a,b;
+    cin>>a>>b;
+    if(a>=b)
+    {
+        cout<<a<<endl;
+    }
+    else
+    {
+        cout<<b<<endl;
+    }
+    }
+          
+     return 0;
+}
