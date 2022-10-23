1858cc7d78aee1e0f2641d3e8e09cf978b8b232e
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-23 23:20:50 +0530

    Create number od credits.cpp

---
+++ number od credits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int s = (4*a) + (2*b) + (0*c);
+	    cout<<s<<endl;
+	}
+	return 0;
+}
