af45944820cfae55378c3174e2092fe0bf42030f
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-23 09:49:22 +0530

    Create rollar coster.cpp

---
+++ rollar coster.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(X>=Y)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	        else{
+	        cout<<"NO"<<endl;
+	        
+	    }
+	}
+	return 0;
+}
