e65f1753ab40b57c0a8e83e4dd05c93e4d7f836f
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-23 14:17:18 +0530

    Create total prize money.cpp

---
+++ total prize money.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        
+            cout<<(10*a + 90*b)<<endl;
+        
+    }
+	
+	return 0;
+}
