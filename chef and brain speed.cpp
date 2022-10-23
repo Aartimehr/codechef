4d7fa0967864f3af5de68dc803f611c6f754a9cf
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-23 13:12:52 +0530

    Create chef and brain speed.cpp

---
+++ chef and brain speed.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=1 && y>=1 && x<=100 && y<=100)
+	    {
+	        if(x>=y)
+	        {
+	            cout<<"NO"<<endl;
+	        }
+	        else
+	        {
+	            cout<<"YES"<<endl;
+	        }
+	        
+	    }
+	    return 0;
+}
