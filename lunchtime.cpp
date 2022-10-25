30dab31d4a8824bb1632c5dacf4469a0e798e13a
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-25 11:23:52 +0530

    Create lunchtime.cpp

---
+++ lunchtime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int a;
+	    cin>>a;
+	    if(a<=4 && a>=1)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else 
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
