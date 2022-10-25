310fb5d98d1aa8bf4bd6ec3daa2d45b280d1c3b1
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-25 14:42:48 +0530

    Create Minimum coins.cpp

---
+++ Minimum coins.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+     while(n--)
+	  {
+	        int a;
+	        cin>>a;
+	        if(a>=1 && a<=1000)
+	        {
+	            cout<<(a%10)<<endl;
+	        }
+	    }
+	return 0;
+}
