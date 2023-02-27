e81a574aa8c55cceffaa55bde34b35354aa09a38
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-02-27 22:38:54 +0530

    Create fitness.cpp

---
+++ fitness.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int T;
+   cin>>T;
+   while(T--)
+   {
+       int X;
+       cin>>X;
+       int m=X*2;
+       cout<<m*5<<endl;
+   }
+
+	return 0;
+}
