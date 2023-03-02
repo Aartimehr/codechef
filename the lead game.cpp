834454b33ae37f35a828cf75df1762ea8da2ccd2
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-03-02 22:12:20 +0530

    Create the lead game.cpp

---
+++ the lead game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	int win=0;
+	int player1=0,player2=0;
+	int lead=0;
+	while(T--)
+	{
+	for(int i=0;i<)
+	    int x,y;
+	    cin>>x>>y;
+	  player1+=x;
+	  player2+=y;
+	  int s=abs(player1-player2);
+	  if(s>lead){
+	      lead=S;
+	  
+	  player1>player2?win=1:win=2;
+	  }
+	}
+	cout<<win<<" "<<lead<<endl;
+	return 0;
+}
+	
